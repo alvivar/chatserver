2066c484584b97e42b0a35b63be38e08d8e48cb3
Andrés Villalobos <andresalvivar@gmail.com>
2023-10-25 05:39:42 -0600

    Simpler.

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,8 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+/** @type {import('tailwindcss').Config} */ module.exports = {
 	content: ['./**/*.{html,js}'],
-	theme: {
-		extend: {},
-	},
+	theme: { extend: {} },
 	plugins: [],
 };

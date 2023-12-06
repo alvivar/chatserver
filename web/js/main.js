54457595708b00d9daffb55897b2a17ba79d81a1
Andrés Villalobos <andresalvivar@gmail.com>
2023-12-05 22:20:13 -0600

    Prettier +1

--- web/js/main.js
+++ web/js/main.js
@@ -1,33 +1,33 @@
-import { PubSub } from './pubsub.js';
-import { connect, send, canReconnect, SocketEvent } from './socket.js';
+import { PubSub } from "./pubsub.js";
+import { connect, send, canReconnect, SocketEvent } from "./socket.js";
 import {
-	appendText,
-	newChat,
-	updateError,
-	startReconLog,
-	clearReconLog,
-	UIEvent,
-} from './ui.js';
+  appendText,
+  newChat,
+  updateError,
+  startReconLog,
+  clearReconLog,
+  UIEvent,
+} from "./ui.js";
 
 PubSub.subscribe(SocketEvent.connected, () => {
-	clearReconLog();
-	updateError('Connected.');
+  clearReconLog();
+  updateError("Connected.");
 });
 
 PubSub.subscribe(SocketEvent.partialMessage, (message) => {
-	appendText(message);
+  appendText(message);
 });
 
 PubSub.subscribe(SocketEvent.completeMessage, (message) => {
-	newChat(message);
+  newChat(message);
 });
 
 PubSub.subscribe(SocketEvent.reconnection, (delay) => {
-	startReconLog(delay, canReconnect);
+  startReconLog(delay, canReconnect);
 });
 
 PubSub.subscribe(UIEvent.sendMessage, (message) => {
-	send(message);
+  send(message);
 });
 
-connect('ws://127.0.0.1:8080/ws');
+connect("ws://127.0.0.1:8080/ws");
--- web/js/socket.js
+++ web/js/socket.js
@@ -1,7 +1,7 @@
-import { PubSub } from './pubsub.js';
+import { PubSub } from "./pubsub.js";
 
 let socket = null;
-let lastAddress = '';
+let lastAddress = "";
 let transmisionComplete = true;
 
 const maxReconnectionAttempts = 30;
@@ -12,67 +12,67 @@ let reconnectionAttempts = 0;
 let reconnectionDelay = baseReconnectionDelay;
 
 const SocketEvent = {
-	connected: 'socket.connected',
-	partialMessage: 'socket.partialMessage',
-	completeMessage: 'socket.completeMessage',
-	reconnection: 'socket.reconnection',
-	error: 'socket.error',
+  connected: "socket.connected",
+  partialMessage: "socket.partialMessage",
+  completeMessage: "socket.completeMessage",
+  reconnection: "socket.reconnection",
+  error: "socket.error",
 };
 
 const connect = (address) => {
-	socket = new WebSocket(address);
-	lastAddress = address;
+  socket = new WebSocket(address);
+  lastAddress = address;
 
-	socket.addEventListener('open', () => {
-		PubSub.publish(SocketEvent.connected, true);
-		reconnectionAttempts = 0;
-		reconnectionDelay = baseReconnectionDelay;
-	});
+  socket.addEventListener("open", () => {
+    PubSub.publish(SocketEvent.connected, true);
+    reconnectionAttempts = 0;
+    reconnectionDelay = baseReconnectionDelay;
+  });
 
-	socket.addEventListener('message', (event) => {
-		if (event.data === '\0') {
-			transmisionComplete = true;
-			return;
-		}
+  socket.addEventListener("message", (event) => {
+    if (event.data === "\0") {
+      transmisionComplete = true;
+      return;
+    }
 
-		if (transmisionComplete) {
-			PubSub.publish(SocketEvent.completeMessage, event.data);
-			transmisionComplete = false;
-		} else {
-			PubSub.publish(SocketEvent.partialMessage, event.data);
-		}
-	});
+    if (transmisionComplete) {
+      PubSub.publish(SocketEvent.completeMessage, event.data);
+      transmisionComplete = false;
+    } else {
+      PubSub.publish(SocketEvent.partialMessage, event.data);
+    }
+  });
 
-	socket.addEventListener('close', () => {
-		if (reconnectionAttempts < maxReconnectionAttempts) {
-			setTimeout(() => {
-				connect(lastAddress);
-			}, reconnectionDelay);
+  socket.addEventListener("close", () => {
+    if (reconnectionAttempts < maxReconnectionAttempts) {
+      setTimeout(() => {
+        connect(lastAddress);
+      }, reconnectionDelay);
 
-			PubSub.publish(SocketEvent.reconnection, reconnectionDelay);
+      PubSub.publish(SocketEvent.reconnection, reconnectionDelay);
 
-			reconnectionAttempts += 1;
-			reconnectionDelay = Math.min(
-				maxReconnectionDelay,
-				reconnectionDelay * 1.5
-			);
-		}
-	});
+      reconnectionAttempts += 1;
+      reconnectionDelay = Math.min(
+        maxReconnectionDelay,
+        reconnectionDelay * 1.5
+      );
+    }
+  });
 
-	socket.addEventListener('error', (event) => {
-		PubSub.publish(SocketEvent.error, event);
-		console.debug(`Error: ${event}`);
-	});
+  socket.addEventListener("error", (event) => {
+    PubSub.publish(SocketEvent.error, event);
+    console.debug(`Error: ${event}`);
+  });
 };
 
 const send = (message) => {
-	if (socket.readyState === WebSocket.OPEN) {
-		socket.send(message);
-	}
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(message);
+  }
 };
 
 const canReconnect = () => {
-	return reconnectionAttempts < maxReconnectionAttempts;
+  return reconnectionAttempts < maxReconnectionAttempts;
 };
 
 export { connect, send, canReconnect, SocketEvent };

11897833d57e14688b362f1b3b0ac6752330cc37
Andrés Villalobos <andresalvivar@gmail.com>
2023-12-07 09:14:24 -0600

    A build script that recreates a build directory and copy the necessary files to run the server.

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Recreates a build directory and copy the necessary files to run the server.
+
+cargo build --release
+cd web/
+npm run build
+cd ..
+
+rm -rf build
+mkdir -p build
+cp .env build/
+cp target/release/chatserver.exe build/
+
+mkdir -p build/web
+cp web/index.html build/web/
+cp web/style.css build/web/
+cp web/favicon.ico build/web/
+
+mkdir -p build/web/js
+cp web/js/main.js build/web/js/
+cp web/js/pubsub.js build/web/js/
+cp web/js/socket.js build/web/js/
+cp web/js/ui.js build/web/js/
